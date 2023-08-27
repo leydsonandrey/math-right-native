a5446192ea21643a675f71b6118f2b507e2d717c
Leydson Andrey <andrey.tar.xz@gmail.com>
2023-08-27 11:45:36 -0300

    Move game to components and add safe area

---
+++ components/Game.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Button } from './Button';
+import { Input } from './Input';
+import { Timer } from './Timer';
+import { SafeArea } from './SafeArea';
+
+export const Game = ({ maximo }) => {
+  //    valor do estado / função de atualização / estado inicial
+  const [input, setInput] = useState('');
+  const [math, setMath] = useState({ n1: 0, n2: 0 });
+  const [change, setChange] = useState(1);
+  const [pontos, setPontos] = useState(0);
+  const [erros, setErros] = useState(0);
+  const [color, setColor] = useState();
+  const [stored, setStored] = useState(0);
+
+  const checkNumber = () => {
+    if (input != (math.n1 + math.n2)) { // errado
+      changeValue()
+      setErros(erros + 1)
+      setColor(false)
+    } else if (input == (math.n1 + math.n2)) { // certo
+      changeValue()
+      setPontos(pontos + 1)
+      setColor(true)
+    } else {
+      alert('erro')
+    }
+  };
+
+  useEffect(() => {
+    setMath({
+      n1: handleRandomNumber(),
+      n2: handleRandomNumber(),
+    });
+  }, [change]) // ao `change` mudar, setMath será executado
+
+  var setMaximo = maximo
+
+  const handleRandomNumber = () => {
+    return Math.floor(Math.random() * (setMaximo - 1 + 1)) + 1;
+  };
+
+  const changeValue = () => {
+    setChange(change + 1)
+    if (pontos < 1) { setPontos(0) }
+    setInput('')
+    setStored(math.n1 + math.n2)
+    setColor()
+  };
+
+  return (
+    <SafeArea background='black'>
+      <View style={styles.gameContainer}>
+        <View>
+          <Text style={styles.calc}>{math.n1} + {math.n2}</Text>
+          <View style={styles.statusContainer}>
+            <Text style={[styles.statusText, styles.statusTrue]}>C {pontos}</Text>
+            <Text style={[styles.statusText, styles.statusFalse]}>E {erros}</Text>
+            <Text style={[styles.statusText, styles.statusPrevious]}>A {stored}</Text>
+            <Timer style={[styles.statusText, styles.statusTimer]} />
+          </View>
+        </View>
+        <Input
+          value={input}
+          placeholder='Calculo'
+          onSubmitEditing={checkNumber}
+          onChangeText={setInput}
+          maxLength={maximo.toString().length}
+          color={color}
+        />
+        <Button
+          onPress={checkNumber}
+          title='Verificar'
+          buttonColor='blue'
+          textColor='white'
+        />
+        <Button
+          onPress={changeValue}
+          title='Gerar Soma'
+          buttonColor='dimgrey'
+          textColor='white'
+        />
+        <StatusBar style="light" />
+      </View>
+    </SafeArea>
+  );
+}
+
+const styles = StyleSheet.create({
+  gameContainer: {
+    gap: 10,
+    paddingTop: 10
+  },
+  statusContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    gap: 10,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  statusText: {
+    fontSize: 16,
+    padding: 5,
+    borderRadius: 5,
+  },
+  statusTrue: {
+    backgroundColor: 'green',
+    color: 'white'
+  },
+  statusFalse: {
+    backgroundColor: 'firebrick',
+    color: 'white'
+  },
+  statusPrevious: {
+    backgroundColor: 'dimgrey',
+    color: 'white'
+  },
+  statusTimer: {
+    backgroundColor: 'blue',
+    color: 'white'
+  },
+  calc: {
+    fontSize: 35,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
+export default Game;
---
+++ components/SafeArea.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+
+export const SafeArea = ({ children, background }) => {
+  return (
+    <SafeAreaView style={styles.container(background)}>
+      <ScrollView>
+        <View style={styles.viewContainer}>
+          {children}
+        </View>
+      </ScrollView>
+    </SafeAreaView >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: (color) => ({
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: color,
+    paddingTop: StatusBar.currentHeight,
+    paddingHorizontal: 20
+  }),
+  viewContainer: {
+    // paddingHorizontal: 20
+  }
+});
+
+export default SafeArea;
