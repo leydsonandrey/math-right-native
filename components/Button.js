94d153078929ad575b7007d1f7b7c9224959a4f3
Leydson Andrey <andrey.tar.xz@gmail.com>
2023-08-27 11:44:49 -0300

    style: button padding 10 to 15

--- components/Button.js
+++ components/Button.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   buttonContainer: (color) => ({
     backgroundColor: color,
     borderRadius: 10,
-    paddingVertical: 10,
+    paddingVertical: 15,
     paddingHorizontal: 10
   }),
   buttonText: (color) => ({
