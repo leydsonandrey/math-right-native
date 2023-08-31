2e64603c523ab9281e91e21f628badee5e42837f
Leydson Andrey <andrey.tar.xz@gmail.com>
2023-08-31 18:54:06 -0300

    `move` app to pages

--- pages/Menu.js
+++ pages/Menu.js
@@ -1,2 +1,24 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+// components
+import { Button } from '../components/Button'
+
 // theme
 import { Colors } from '../theme/Colors'
+
+export const Menu = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', gap: 10, backgroundColor: Colors.black, paddingHorizontal: 20, paddingTop: StatusBar.currentHeight }}>
+      <Text style={{ color: 'white', textAlign: 'center', fontSize: 50, marginBottom: 50 }}>Math Right</Text>
+      <Button
+        title="Jogar"
+        onPress={() => navigation.push('Modos')}
+        buttonColor={Colors.blue}
+        textColor='white'
+      />
+      <StatusBar theme='light' />
+    </View>
+  );
+}
