d44a2b6634c2a430c90cc6665f9ad72a201fdcef
Leydson Andrey <andrey.tar.xz@gmail.com>
2023-09-08 09:14:23 -0300

    `style` maxWidth is 500

--- components/SafeArea.js
+++ components/SafeArea.js
@@ -65,7 +65,7 @@ const styles = StyleSheet.create({
   }),
   viewContainer: (notScrollView) => ({
     width: '100%',
-    maxWidth: 720,
+    maxWidth: 500,
     paddingHorizontal: 20,
     paddingBottom: 20,
     marginTop: notScrollView ? 0 : 20,
